1ad3dfbc69e718b787bc9ddc0461abf20faf2e53
PBAB0SS <nick_nolte@pba.edu>
2019-09-03 19:25:16 -0400

    MiniProject 19

---
+++ Final Averages For Semester/Final Averages For Semester/Final Averages For Semester/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_Enter = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(448, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(432, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Final Averages For Semester"
+        '
+        'btn_Enter
+        '
+        Me.btn_Enter.Location = New System.Drawing.Point(484, 265)
+        Me.btn_Enter.Name = "btn_Enter"
+        Me.btn_Enter.Size = New System.Drawing.Size(252, 105)
+        Me.btn_Enter.TabIndex = 2
+        Me.btn_Enter.Text = "Enter Grades"
+        Me.btn_Enter.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(1132, 49)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(588, 691)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1873, 1184)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btn_Enter)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "lbl_Title"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btn_Enter As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Final Averages For Semester/Final Averages For Semester/Final Averages For Semester/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Private Sub btn_Enter_Click(sender As Object, e As EventArgs) Handles btn_Enter.Click
+        Dim Grades(12) As Integer
+        Dim Input As Integer
+        Dim Count As Integer = 0
+        Dim Number As Integer = 1
+
+        While Count <= 12
+            Input = InputBox("Enter a Number #" & Number)
+            Grades(Count) = Input
+            Count += 1
+            Number += 1
+        End While
+
+        Array.Sort(Grades)
+
+        Dim ObjWriter As IO.StreamWriter
+        Dim File As String = "C:\Users\big_n\OneDrive - Palm Beach Atlantic University\Visual Studio 2012\Projects\Final Averages For Semester\Final Averages For Semester\Grades.txt"
+        Dim Average As Decimal = (Grades(2) + Grades(3) + Grades(4) + Grades(5) + Grades(6) + Grades(7) + Grades(8) + Grades(9) + Grades(10) + Grades(12)) / 11
+
+        ObjWriter = My.Computer.FileSystem.OpenTextFileWriter(File, False)
+
+        For Each element In Grades
+            ObjWriter.WriteLine(element)
+        Next
+
+        ObjWriter.WriteLine(Environment.NewLine & "Average: " & Average.ToString("F2") & "%")
+        ObjWriter.Close()
+
+    End Sub
+End Class

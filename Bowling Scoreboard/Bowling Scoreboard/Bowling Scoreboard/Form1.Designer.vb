0d36b651f1c3efacac73bc6ccd8f1fd1f6fb3c4d
PBAB0SS <nick_nolte@pba.edu>
2019-09-03 19:13:43 -0400

    MiniProject 13

---
+++ Bowling Scoreboard/Bowling Scoreboard/Bowling Scoreboard/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.lstScore = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(318, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(272, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Frame-by-Frame Scoring"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.Location = New System.Drawing.Point(87, 101)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(172, 23)
+        Me.btnEnter.TabIndex = 1
+        Me.btnEnter.Text = "Enter Each Number"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'lstScore
+        '
+        Me.lstScore.FormattingEnabled = True
+        Me.lstScore.Location = New System.Drawing.Point(64, 142)
+        Me.lstScore.Name = "lstScore"
+        Me.lstScore.Size = New System.Drawing.Size(195, 225)
+        Me.lstScore.TabIndex = 2
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(873, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Location = New System.Drawing.Point(529, 354)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(0, 13)
+        Me.lblTotal.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(873, 523)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lstScore)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnEnter As System.Windows.Forms.Button
+    Friend WithEvents lstScore As System.Windows.Forms.ListBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+
+End Class
---
+++ Bowling Scoreboard/Bowling Scoreboard/Bowling Scoreboard/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Dim InputCancelled As String = ""
+
+        Dim strBowling As String
+
+        Dim BowlingScore, Score As Integer
+        Dim intnumberofentries As Integer = 1
+        Dim intmaxnumberofentries As Integer = 10
+        Dim RunningTotal As Integer = 0
+
+
+        strBowling = InputBox("Enter Score #" & intnumberofentries, "Score Total", " ")
+
+
+
+        Do Until intnumberofentries > intmaxnumberofentries Or strBowling = InputCancelled
+            If IsNumeric(strBowling) Then
+                BowlingScore = Convert.ToInt64(strBowling)
+                If BowlingScore >= 0 Then
+                    Score += BowlingScore
+                    lstScore.Items.Add("Frame Score " & BowlingScore & " Total Score " & Score)
+                    RunningTotal += BowlingScore
+                    intnumberofentries += 1
+
+
+                Else
+                    MsgBox("Enter a Positive Number for Score")
+                End If
+
+
+            Else
+                MsgBox("Enter a Number for Score")
+            End If
+            If intnumberofentries <= intmaxnumberofentries Then
+                strBowling = InputBox("Enter Score #" & intnumberofentries, "Score Total", " ")
+
+            End If
+
+        Loop
+        Dim ScoreTotal As Decimal
+
+
+        If intnumberofentries > 1 Then
+            ScoreTotal = (Score)
+            lblTotal.Text = "Final Game score is " & ScoreTotal
+        Else
+            MsgBox("Enter a Number to Begin")
+        End If
+    End Sub
+End Class

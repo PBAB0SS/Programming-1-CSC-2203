29b61b7964221f8729cd6740b058fb19f2924bdc
PBAB0SS <nick_nolte@pba.edu>
2019-09-03 19:12:44 -0400

    MiniProject 12

---
+++ Corn Maze Average Time/Corn Maze Average Time/Corn Maze Average Time/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lbl_Title = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.lbl_Output = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl_Title
+        '
+        Me.lbl_Title.AutoSize = True
+        Me.lbl_Title.BackColor = System.Drawing.Color.Sienna
+        Me.lbl_Title.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl_Title.Location = New System.Drawing.Point(265, 95)
+        Me.lbl_Title.Name = "lbl_Title"
+        Me.lbl_Title.Size = New System.Drawing.Size(427, 37)
+        Me.lbl_Title.TabIndex = 0
+        Me.lbl_Title.Text = "Race through the Corn Maze"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SaddleBrown
+        Me.Button1.Location = New System.Drawing.Point(286, 197)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(363, 97)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Enter Completion Time"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.ForeColor = System.Drawing.Color.Sienna
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 55
+        Me.ListBox1.Location = New System.Drawing.Point(338, 323)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(216, 719)
+        Me.ListBox1.TabIndex = 2
+        '
+        'lbl_Output
+        '
+        Me.lbl_Output.AutoSize = True
+        Me.lbl_Output.BackColor = System.Drawing.Color.Sienna
+        Me.lbl_Output.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl_Output.Location = New System.Drawing.Point(62, 1160)
+        Me.lbl_Output.Name = "lbl_Output"
+        Me.lbl_Output.Size = New System.Drawing.Size(0, 37)
+        Me.lbl_Output.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(997, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(499, 1366)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1508, 1405)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lbl_Output)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbl_Title)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_Title As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents lbl_Output As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Corn Maze Average Time/Corn Maze Average Time/Corn Maze Average Time/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strCornMazeTime As String
+        Dim strcanelclicked As String = ""
+        Dim decCornMazeTime, decTotalCornMazeTime As Decimal
+
+        Dim inNumberOfEntries As Integer = 1
+        Dim intMaxNumberOfEntries As Integer = 10
+
+        strCornMazeTime = InputBox("Enter the Compleation Time for Participant # " & inNumberOfEntries, "Corn Maze")
+
+        Do Until inNumberOfEntries > intMaxNumberOfEntries Or strCornMazeTime = strcanelclicked
+            If IsNumeric(strCornMazeTime) Then
+                decCornMazeTime = Convert.ToDecimal(strCornMazeTime)
+                If decCornMazeTime > 0 Then
+                    decTotalCornMazeTime += decCornMazeTime
+                    inNumberOfEntries = inNumberOfEntries + 1
+                    ListBox1.Items.Add(decCornMazeTime)
+                Else
+                    MsgBox("Error - Enter a Postivie Number for the " & inNumberOfEntries, MsgBoxStyle.OkOnly, "Weight Loss")
+                End If
+            Else
+                MsgBox("Error - Enter Time for Corn Maze # " & inNumberOfEntries, MsgBoxStyle.OkOnly, "Weight Loss")
+            End If
+            If inNumberOfEntries <= intMaxNumberOfEntries Then
+                strCornMazeTime = InputBox("Enter the Compleation Time for Participant # " & inNumberOfEntries, "Corn Maze")
+            End If
+        Loop
+        Dim DecAverageLoss As Decimal
+
+        If inNumberOfEntries > 1 Then
+            DecAverageLoss = decTotalCornMazeTime / (inNumberOfEntries - 1)
+            lbl_Output.Text = "The Average Completion Time is " & DecAverageLoss.ToString("F1")
+
+        Else
+            MsgBox("No Corn Maze Times Were Entereed")
+        End If
+    End Sub
+End Class

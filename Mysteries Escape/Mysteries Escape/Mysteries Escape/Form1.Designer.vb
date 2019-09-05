fa2802ead2e9ebe651773f666642de0c7d932ecf
PBAB0SS <nick_nolte@pba.edu>
2019-09-05 00:17:44 -0400

    In Class Project

---
+++ Mysteries Escape/Mysteries Escape/Mysteries Escape/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_Title = New System.Windows.Forms.Label()
+        Me.cbo_City = New System.Windows.Forms.ComboBox()
+        Me.lbl_Team = New System.Windows.Forms.Label()
+        Me.txt_Team = New System.Windows.Forms.TextBox()
+        Me.lbl_Game = New System.Windows.Forms.Label()
+        Me.lst_Game = New System.Windows.Forms.ListBox()
+        Me.btn_CliantCost = New System.Windows.Forms.Button()
+        Me.btn_Clear = New System.Windows.Forms.Button()
+        Me.lbl_GameType = New System.Windows.Forms.Label()
+        Me.lbl_Cost = New System.Windows.Forms.Label()
+        Me.lbl_Length = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_Title
+        '
+        Me.lbl_Title.AutoSize = True
+        Me.lbl_Title.Location = New System.Drawing.Point(306, 29)
+        Me.lbl_Title.Name = "lbl_Title"
+        Me.lbl_Title.Size = New System.Drawing.Size(396, 37)
+        Me.lbl_Title.TabIndex = 0
+        Me.lbl_Title.Text = "Myster Escape Information"
+        '
+        'cbo_City
+        '
+        Me.cbo_City.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_City.FormattingEnabled = True
+        Me.cbo_City.Items.AddRange(New Object() {"Dallas", "Paris", "Singapor"})
+        Me.cbo_City.Location = New System.Drawing.Point(443, 112)
+        Me.cbo_City.Name = "cbo_City"
+        Me.cbo_City.Size = New System.Drawing.Size(371, 45)
+        Me.cbo_City.TabIndex = 1
+        '
+        'lbl_Team
+        '
+        Me.lbl_Team.AutoSize = True
+        Me.lbl_Team.Location = New System.Drawing.Point(242, 191)
+        Me.lbl_Team.Name = "lbl_Team"
+        Me.lbl_Team.Size = New System.Drawing.Size(266, 37)
+        Me.lbl_Team.TabIndex = 2
+        Me.lbl_Team.Text = "Number in Team:"
+        '
+        'txt_Team
+        '
+        Me.txt_Team.Location = New System.Drawing.Point(563, 191)
+        Me.txt_Team.Name = "txt_Team"
+        Me.txt_Team.Size = New System.Drawing.Size(100, 44)
+        Me.txt_Team.TabIndex = 3
+        '
+        'lbl_Game
+        '
+        Me.lbl_Game.AutoSize = True
+        Me.lbl_Game.Location = New System.Drawing.Point(242, 308)
+        Me.lbl_Game.Name = "lbl_Game"
+        Me.lbl_Game.Size = New System.Drawing.Size(288, 37)
+        Me.lbl_Game.TabIndex = 4
+        Me.lbl_Game.Text = "Select Game Type:"
+        '
+        'lst_Game
+        '
+        Me.lst_Game.FormattingEnabled = True
+        Me.lst_Game.ItemHeight = 37
+        Me.lst_Game.Location = New System.Drawing.Point(249, 348)
+        Me.lst_Game.Name = "lst_Game"
+        Me.lst_Game.Size = New System.Drawing.Size(304, 263)
+        Me.lst_Game.TabIndex = 5
+        '
+        'btn_CliantCost
+        '
+        Me.btn_CliantCost.Location = New System.Drawing.Point(184, 660)
+        Me.btn_CliantCost.Name = "btn_CliantCost"
+        Me.btn_CliantCost.Size = New System.Drawing.Size(276, 68)
+        Me.btn_CliantCost.TabIndex = 6
+        Me.btn_CliantCost.Text = "Find Team Cost"
+        Me.btn_CliantCost.UseVisualStyleBackColor = True
+        '
+        'btn_Clear
+        '
+        Me.btn_Clear.Location = New System.Drawing.Point(525, 660)
+        Me.btn_Clear.Name = "btn_Clear"
+        Me.btn_Clear.Size = New System.Drawing.Size(164, 68)
+        Me.btn_Clear.TabIndex = 7
+        Me.btn_Clear.Text = "Clear Form"
+        Me.btn_Clear.UseVisualStyleBackColor = True
+        '
+        'lbl_GameType
+        '
+        Me.lbl_GameType.AutoSize = True
+        Me.lbl_GameType.Location = New System.Drawing.Point(194, 802)
+        Me.lbl_GameType.Name = "lbl_GameType"
+        Me.lbl_GameType.Size = New System.Drawing.Size(113, 37)
+        Me.lbl_GameType.TabIndex = 8
+        Me.lbl_GameType.Text = "Game:"
+        '
+        'lbl_Cost
+        '
+        Me.lbl_Cost.AutoSize = True
+        Me.lbl_Cost.Location = New System.Drawing.Point(194, 883)
+        Me.lbl_Cost.Name = "lbl_Cost"
+        Me.lbl_Cost.Size = New System.Drawing.Size(92, 37)
+        Me.lbl_Cost.TabIndex = 9
+        Me.lbl_Cost.Text = "Cost:"
+        '
+        'lbl_Length
+        '
+        Me.lbl_Length.AutoSize = True
+        Me.lbl_Length.Location = New System.Drawing.Point(194, 966)
+        Me.lbl_Length.Name = "lbl_Length"
+        Me.lbl_Length.Size = New System.Drawing.Size(124, 37)
+        Me.lbl_Length.TabIndex = 10
+        Me.lbl_Length.Text = "Length:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Label1.Location = New System.Drawing.Point(445, 115)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(307, 37)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Select City Location:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(1127, 1032)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_Length)
+        Me.Controls.Add(Me.lbl_Cost)
+        Me.Controls.Add(Me.lbl_GameType)
+        Me.Controls.Add(Me.btn_Clear)
+        Me.Controls.Add(Me.btn_CliantCost)
+        Me.Controls.Add(Me.lst_Game)
+        Me.Controls.Add(Me.lbl_Game)
+        Me.Controls.Add(Me.txt_Team)
+        Me.Controls.Add(Me.lbl_Team)
+        Me.Controls.Add(Me.cbo_City)
+        Me.Controls.Add(Me.lbl_Title)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_Title As System.Windows.Forms.Label
+    Friend WithEvents cbo_City As System.Windows.Forms.ComboBox
+    Friend WithEvents lbl_Team As System.Windows.Forms.Label
+    Friend WithEvents txt_Team As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_Game As System.Windows.Forms.Label
+    Friend WithEvents lst_Game As System.Windows.Forms.ListBox
+    Friend WithEvents btn_CliantCost As System.Windows.Forms.Button
+    Friend WithEvents btn_Clear As System.Windows.Forms.Button
+    Friend WithEvents lbl_GameType As System.Windows.Forms.Label
+    Friend WithEvents lbl_Cost As System.Windows.Forms.Label
+    Friend WithEvents lbl_Length As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Mysteries Escape/Mysteries Escape/Mysteries Escape/Form1.vb
@@ -0,0 +1,205 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btn_Clear.Enabled = False
+    End Sub
+
+    Private Sub cbo_City_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbo_City.SelectedIndexChanged
+        Dim intCityChoice As Integer
+
+        Label1.Visible = False
+
+        intCityChoice = cbo_City.SelectedIndex
+        lst_Game.Items.Clear()
+
+        Select Case intCityChoice
+            Case 0
+                DallasTeam()
+            Case 1
+                ParisTeam()
+            Case 2
+                SingaporeTeam()
+        End Select
+    End Sub
+
+    Private Sub DallasTeam()
+        lst_Game.Items.Add("Spy in the Study")
+        lst_Game.Items.Add("Hidden Cellar")
+        lst_Game.Items.Add("Team Building")
+    End Sub
+
+    Private Sub ParisTeam()
+        lst_Game.Items.Add("The Encounter")
+        lst_Game.Items.Add("Library Larceny")
+    End Sub
+
+    Private Sub SingaporeTeam()
+        lst_Game.Items.Add("Hidden Cellar")
+        lst_Game.Items.Add("Team Building")
+        lst_Game.Items.Add("Amazing Race")
+    End Sub
+
+    Private Sub lst_Game_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lst_Game.SelectedIndexChanged
+        btn_Clear.Enabled = True
+    End Sub
+
+    Private Sub btn_CliantCost_Click(sender As Object, e As EventArgs) Handles btn_CliantCost.Click
+        Dim blnnumberinTeamisValid As Boolean = False
+        Dim intGameChoice As Integer
+        Dim blnGameisSelected As Boolean = False
+        Dim strSelectedItem As String = ""
+        Dim intTeamSize As Integer
+        Dim intCityChoice As Integer
+        Dim decTotalCost As Decimal
+        Dim intLength As Integer = 0
+
+        blnnumberinTeamisValid = ValidateNumberinTeam()
+
+
+        intGameChoice = ValidateGameSelection(blnGameisSelected, strSelectedItem)
+
+        If (blnnumberinTeamisValid And blnGameisSelected) Then
+            intTeamSize = Convert.ToInt32(txt_Team.Text)
+            intCityChoice = cbo_City.SelectedIndex
+
+            Select Case intCityChoice
+                Case 0
+                    decTotalCost = DallasFindCost(intGameChoice, intTeamSize, intLength)
+                Case 1
+                    decTotalCost = ParisFindCost(intGameChoice, intTeamSize, intLength)
+                Case 2
+                    decTotalCost = SingaporeFindCost(intGameChoice, intTeamSize, intLength)
+
+            End Select
+            lbl_GameType.Text = "Game: " & strSelectedItem
+            lbl_Cost.Text = "Cost: " & decTotalCost.ToString("C")
+            lbl_Length.Text = "Length: " & intLength.ToString() & " Hour(s)"
+        End If
+
+
+    End Sub
+
+    Private Function ValidateNumberinTeam() As Boolean
+        Dim intTeamSize As Integer
+        Dim blnValidityCheck As Boolean = False
+
+        Try
+            intTeamSize = Convert.ToInt16(txt_Team.Text)
+
+            If intTeamSize >= 2 And intTeamSize <= 6 Then
+                blnValidityCheck = True
+            Else
+                MsgBox("Please enter the number of people in your team (2-6)", MsgBoxStyle.OkOnly, "Error")
+                txt_Team.Clear()
+                txt_Team.Focus()
+            End If
+        Catch excetion As FormatException
+            MsgBox("Please enter the number of people in your team (2-6)", MsgBoxStyle.OkOnly, "Error")
+            txt_Team.Clear()
+            txt_Team.Focus()
+        Catch excetion As OverflowException
+            MsgBox("Please enter the number of people in your team (2-6)", MsgBoxStyle.OkOnly, "Error")
+            txt_Team.Clear()
+            txt_Team.Focus()
+        Catch excetion As SystemException
+            MsgBox("Please enter the number of people in your team (2-6)", MsgBoxStyle.OkOnly, "Error")
+            txt_Team.Clear()
+            txt_Team.Focus()
+        End Try
+
+        Return blnValidityCheck
+
+    End Function
+
+    Private Function dallasFindCost(ByVal intGame As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Decimal
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+
+        Select Case intGame
+            Case 0
+                decTeamCost = 32D
+                intTime = 1
+            Case 1
+                decTeamCost = 39D
+                intTime = 1
+            Case 2
+                decTeamCost = 55D
+                intTime = 2
+        End Select
+        decFinalCost = decTeamCost * intTeam
+
+        Return decFinalCost
+
+    End Function
+
+    Private Function ParisFindCost(ByVal intGame As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Decimal
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+
+        Select Case intGame
+            Case 0
+                decTeamCost = 38D
+                intTime = 1
+            Case 1
+                decTeamCost = 45D
+                intTime = 1
+        End Select
+        decFinalCost = decTeamCost * intTeam
+
+        Return decFinalCost
+
+    End Function
+
+    Private Function SingaporeFindCost(ByVal intGame As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Decimal
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+
+        Select Case intGame
+            Case 0
+                decTeamCost = 45D
+                intTime = 1
+            Case 1
+                decTeamCost = 65D
+                intTime = 2
+            Case 2
+                decTeamCost = 75D
+                intTime = 4
+        End Select
+        decFinalCost = decTeamCost * intTeam
+
+        Return decFinalCost
+
+    End Function
+
+    Private Function ValidateGameSelection(ByRef blnGame As Boolean, ByRef strGame As String) As Integer
+        Dim GameType As Integer
+
+        Try
+            GameType = Convert.ToInt32(lst_Game.SelectedIndex)
+            strGame = lst_Game.SelectedItem.ToString()
+            blnGame = True
+        Catch Exception As SystemException
+            MsgBox("Select a Game Type", , "Error")
+            blnGame = False
+        End Try
+
+        Return GameType
+
+    End Function
+
+    Private Sub btn_Clear_Click(sender As Object, e As EventArgs) Handles btn_Clear.Click
+        cbo_City.SelectedIndex = -1
+
+        Label1.Visible = True
+        lbl_GameType.Text = "Game:"
+        lbl_Length.Text = "Length:"
+        lbl_Cost.Text = "Cost:"
+        txt_Team.Clear()
+        lst_Game.Items.Clear()
+        btn_Clear.Enabled = False
+
+
+
+    End Sub
+
+End Class
